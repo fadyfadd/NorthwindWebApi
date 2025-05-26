3fb8232d2db2d0e8d81f845fd82174dfa82fa7a3
fadyfadd <fady_faddoul@hotmail.com>
2025-05-26 18:59:57 +0300

    Cleanup

--- NorthwindWebApi/ErrorMessages.cs
+++ NorthwindWebApi/ErrorMessages.cs
@@ -4,8 +4,7 @@ public static class ErrorMessages
 {
     public const String UserNotFound = "User not found";
     public const String UserRoleNotFound = "User Role not found";
-    public const String NonValidRoleName = "Non-valid role name";
     public const String AuthenticationError = "Authentication error";
-    public const String RoleNotValid = "Role not valid";
+    public const String RoleNotValid = "User Role not valid";
     
 }
