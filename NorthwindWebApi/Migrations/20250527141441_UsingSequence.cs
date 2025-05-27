ef0a963ddb8655064aaa798f3cccbbdcc9ecf5c5
fadyfadd <fady_faddoul@hotmail.com>
2025-05-27 20:53:58 +0300

    Migrations

---
+++ NorthwindWebApi/Migrations/20250527141441_UsingSequence.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NorthwindWebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsingSequence : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "supplier_id",
+                table: "suppliers",
+                type: "integer",
+                nullable: false,
+                defaultValueSql: "nextval('\"product_sequence\"')",
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValueSql: "nextval('product_sequence')");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "product_id",
+                table: "products",
+                type: "integer",
+                nullable: false,
+                defaultValueSql: "nextval('\"product_sequence\"')",
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValueSql: "nextval('product_sequence')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "supplier_id",
+                table: "suppliers",
+                type: "integer",
+                nullable: false,
+                defaultValueSql: "nextval('product_sequence')",
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValueSql: "nextval('\"product_sequence\"')");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "product_id",
+                table: "products",
+                type: "integer",
+                nullable: false,
+                defaultValueSql: "nextval('product_sequence')",
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValueSql: "nextval('\"product_sequence\"')");
+        }
+    }
+}
---
+++ NorthwindWebApi/Migrations/20250527142404_FieldNullability.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NorthwindWebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class FieldNullability : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "postal_code",
+                table: "suppliers",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "phone",
+                table: "suppliers",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "country",
+                table: "suppliers",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "units_on_order",
+                table: "products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "units_in_stock",
+                table: "products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "unit_price",
+                table: "products",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "reorder_level",
+                table: "products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "postal_code",
+                table: "suppliers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "phone",
+                table: "suppliers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "country",
+                table: "suppliers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "units_on_order",
+                table: "products",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "units_in_stock",
+                table: "products",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "unit_price",
+                table: "products",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "reorder_level",
+                table: "products",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+    }
+}
